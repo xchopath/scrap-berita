9e2400bc4bfc6da1ebe1cdf4320dae23455522d9
NFA <44427665+xchopath@users.noreply.github.com>
2022-07-21 20:29:27 +0700

    Update tempodotco-scrap.py

--- scripts/tempodotco-scrap.py
+++ scripts/tempodotco-scrap.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-KEYWORD = 'hacker'
+KEYWORD = 'GANTENG'
 
 headers = {
 	'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.51 Mobile Safari/537.36'
@@ -17,20 +17,15 @@ list_berita = soup.find_all("h3", {"class": "title line3"})
 
 for berita in list_berita:
 	soup = BeautifulSoup(str(berita), "lxml")
-	link = soup.find('a')
+	link = soup.find('a')['href']
 	try:
-		open_link = requests.get(link['href'], headers=headers)
+		open_link = requests.get(link, headers=headers)
 		soup = BeautifulSoup(open_link.text, "lxml")
-		title = soup.find('meta', attrs={'property':'og:title'})
-		title = title['content']
-		published = soup.find('meta', attrs={'property':'article:published_time'})
-		published = published['content']
-		author = soup.find('meta', attrs={'name':'author'})
-		author = author['content']
-		url = soup.find('meta', attrs={'property':'og:url'})
-		url = url['content']
-		img = soup.find('meta', attrs={'property':'og:image'})
-		img = img['content']
+		title = soup.find('meta', attrs={'property':'og:title'})['content']
+		published = soup.find('meta', attrs={'property':'article:published_time'})['content']
+		author = soup.find('meta', attrs={'name':'author'})['content']
+		url = soup.find('meta', attrs={'property':'og:url'})['content']
+		img = soup.find('meta', attrs={'property':'og:image'})['content']
 		content = soup.find("div", {"id": "isi"})
 		content = str(content).replace("<", " <")
 		content = BeautifulSoup(content, "lxml")
